7a8f2c0d9d3740ce35ff3dbc47bca840eb651107
Xel'Bayria <12866666+Xelbayria@users.noreply.github.com>
2024-11-22 01:09:45 -0700

    ExampleMod: Corrected MOD_ID

--- common/src/main/java/net/xelbayria/examplemod/ExampleMod.java
+++ common/src/main/java/net/xelbayria/examplemod/ExampleMod.java
@@ -6,7 +6,7 @@ import com.mojang.logging.LogUtils;
 
 public class ExampleMod
 {
-    public static final String MOD_ID = "example";
+    public static final String MOD_ID = "examplemod";
     
     public static final Logger LOGGER = LogUtils.getLogger();
 
